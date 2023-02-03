265c7ed61387fe0e14901a7e4c6b54f03bfd62b1
George <efimenkoipad@icloud.com>
2023-02-03 20:08:01 +0300

    third commit

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
 public class Calculator {
-    //    Исключения
     static class CalculatorException extends Exception {
         public CalculatorException(String message) {
             super(message);
         }
     }
-    //    Метод для определения типа цифр
     static int arabicOrRomans(String[] array) throws CalculatorException {
         String[] arabics = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
         String[] romans = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
@@ -31,7 +29,6 @@ public class Calculator {
         }
         return a;
     }
-    //    Метод для конвертации арабских цифр в римские
     static String resultConvertToRomans(int result) {
         int[] arabics = {100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romans = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
@@ -49,13 +46,10 @@ public class Calculator {
         }
         return new String(output);
     }
-    //    CАМ КАКЛЬКУЛЯТОР
     public static String calc(String input) throws CalculatorException {
-//        Получаем string массив с цифрами и приводим его в вид для удобной работы, добавляем еще один массив для вывода результата
         String[] stringArray;
         stringArray = input.toUpperCase().replaceAll("\\s", "").split("[+\\-*/]",2);
         if(stringArray.length<2){throw new CalculatorException("Введено некорректное выражение");}
-//        Проверяем введенные цифры (римские или арабские) и добавляем их в массив
         int[] digits = new int[2];
         int forResultOutput=0;
         if (arabicOrRomans(stringArray) == 1) {
@@ -86,16 +80,13 @@ public class Calculator {
                 digits[i] = Integer.parseInt(stringArray[i]);
             }
         }
-//        Проверка на диапазон цифр 1 - 10
         for (int d : digits) {
             if (d < 1 || d > 10) {
                 throw new CalculatorException("Введено некорректное выражение");
             }
         }
-        //Присваиваем переменным значения, полученные из массива int
         int a = digits[0];
         int b = digits[1];
-        //Определяем оператор, указанный в выражении и выполняем расчеты
         int result = 0;
         char[] operator = input.toCharArray();
         for (int i = 0; i < input.length(); i++) {
@@ -106,7 +97,6 @@ public class Calculator {
                 case '/' -> result = a / b;
             }
         }
-        //Выводим результат, сделав его строкой
         if(forResultOutput==2){
             if(result<1){
                 throw new CalculatorException("В римской системе счисления нет отрицательных чисел или ответ не м.б. равен 0");
