9deb94d0da11137bd555617b7437e314ee7b0d96
George <efimenkoipad@icloud.com>
2023-02-25 00:03:37 +0300

    test commit

---
+++ calculator/src/Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+    public static int summa(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        System.out.println( summa(a,b));
+    }
+}
